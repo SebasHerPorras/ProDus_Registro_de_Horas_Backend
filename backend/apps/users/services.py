5f981683b4bb19157b5ee26cd59c1225ff7b5b98
Esteban Chacón <180124256+luisEChaconC@users.noreply.github.com>
2026-04-08 13:31:49 -0600

    add username_exists to check for existing usernames

--- backend/apps/users/services.py
+++ backend/apps/users/services.py
@@ -1,5 +1,9 @@
+from .models import User
 from .validators import get_client_ip, is_valid_institute_ip
 
+
+def username_exists(username) -> bool:
+    return User.objects.filter(username=username).exists()
 
 def validate_institute_ip_addr(request):
     client_ip = get_client_ip(request)
