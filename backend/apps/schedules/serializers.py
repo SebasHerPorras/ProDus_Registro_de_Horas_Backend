2f80a9beb44421b0d9b2ce5d8e1e01337d0f064e
Esteban Chacón <180124256+luisEChaconC@users.noreply.github.com>
2026-04-17 13:04:46 -0600

    feat: define ScheduleCreateSerializer for schedule creation

--- backend/apps/schedules/serializers.py
+++ backend/apps/schedules/serializers.py
@@ -4,3 +4,9 @@ class ScheduleBlockCreateSerializer(serializers.Serializer):
     day_of_week = serializers.CharField(max_length=15)
     start_time = serializers.TimeField()
     end_time = serializers.TimeField()
+
+class ScheduleCreateSerializer(serializers.Serializer):
+    assistant = serializers.IntegerField()
+    valid_from = serializers.DateField()
+    valid_to = serializers.DateField(required=False, allow_null=True)
+    blocks = ScheduleBlockCreateSerializer(many=True)
