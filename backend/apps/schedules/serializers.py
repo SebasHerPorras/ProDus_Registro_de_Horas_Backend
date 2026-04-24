908870dcf37af3978923126ecd740ae4f94b755f
Esteban Chacón <180124256+luisEChaconC@users.noreply.github.com>
2026-04-24 12:16:30 -0600

    turn day_of_week attribute into ChoiceField

--- backend/apps/schedules/serializers.py
+++ backend/apps/schedules/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
 class ScheduleBlockCreateSerializer(serializers.Serializer):
-    day_of_week = serializers.CharField(max_length=15)
+    day_of_week = serializers.ChoiceField(ScheduleBlock.DayOfWeek)
     start_time = serializers.TimeField()
     end_time = serializers.TimeField()
